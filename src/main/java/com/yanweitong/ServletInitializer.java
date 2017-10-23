a06a345b75d97781c607a321ba8bb4d923b4eda2
weitongyan <yanweitong.tim@gmail.com>
2017-10-24 00:48:18 +0800

    add the .gitignore and src

---
+++ src/main/java/com/yanweitong/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.yanweitong;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(YanweitongApplication.class);
+	}
+
+}
---
+++ src/main/java/com/yanweitong/YanweitongApplication.java
@@ -0,0 +1,12 @@
+package com.yanweitong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YanweitongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YanweitongApplication.class, args);
+	}
+}
